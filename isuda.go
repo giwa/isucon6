fdd1b5afa65bf612c9be0994da31fdc637f13974
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-09-16 00:08:56 +0900

    Quic kome 2

--- isuda.go
+++ isuda.go
@@ -292,9 +292,9 @@ func keywordByKeywordDeleteHandler(w http.ResponseWriter, r *http.Request) {
 		badRequest(w)
 		return
 	}
-	row := db.QueryRow(`SELECT * FROM entry WHERE keyword = ?`, keyword)
-	e := Entry{}
-	err := row.Scan(&e.ID, &e.AuthorID, &e.Keyword, &e.Description, &e.UpdatedAt, &e.CreatedAt)
+	row := db.QueryRow(`SELECT keyword FROM entry WHERE keyword = ?`, keyword)
+	k := Keyword{}
+	err := row.Scan(&k.Keyword)
 	if err == sql.ErrNoRows {
 		notFound(w)
 		return
